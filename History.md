71186234cbbfb22514caf4b92d1bbcf0b90ef6d5
Titus Wormer <tituswormer@gmail.com>
2015-05-03 11:52:21 +0200

    0.1.1

--- History.md
+++ History.md
@@ -1,3 +1,8 @@
+0.1.1 / 2015-05-01
+==================
+
+*   Fix malformed `package.json` ([67cdd7e](https://github.com/wooorm/levenshtein.c/commit/67cdd7e))
+
 0.1.0 / 2015-05-01
 ==================
 
--- levenshtein-cli.c
+++ levenshtein-cli.c
@@ -13,7 +13,7 @@ main(int argc, char **argv) {
 
     if (argc == 2) {
         if (!strcmp(a, "-v") || !strcmp(a, "--version")) {
-            printf("%s", "0.1.0\n");
+            printf("%s", "0.1.1\n");
 
             return 0;
         }
