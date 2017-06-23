54a0d3ab87a2a68e232b02117a972f5909c4856b
Titus Wormer <tituswormer@gmail.com>
2017-06-23 11:31:11 +0200

    Update `levenshtein.c`

--- deps/levenshtein.c/levenshtein.h
+++ deps/levenshtein.c/levenshtein.h
@@ -1,18 +1,24 @@
-/**
- * `levenshtein.h` - levenshtein
- *
- * MIT licensed.
- *
- * Copyright (c) 2015 Titus Wormer <tituswormer@gmail.com>
- */
+#ifndef LEVENSHTEIN_H
+#define LEVENSHTEIN_H
 
-/**
- * Returns an unsigned integer, depicting
+/* `levenshtein.h` - levenshtein
+ * MIT licensed.
+ * Copyright (c) 2015 Titus Wormer <tituswormer@gmail.com> */
+
+/* Returns an unsigned integer, depicting
  * the difference between `a` and `b`.
- *
  * See http://en.wikipedia.org/wiki/Levenshtein_distance
  * for more information.
  */
-
+#ifdef __cplusplus
+extern "C" {
+#endif
+  
 unsigned int
 levenshtein(const char *a, const char *b);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // LEVENSHTEIN_H
